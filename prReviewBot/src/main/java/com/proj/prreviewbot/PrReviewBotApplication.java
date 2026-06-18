64e15195127be554279dfee007d94126f71bca6b
rachit-890 <rachitkushwaha890@gmail.com>
2026-06-19 03:18:31 +0530

    initial commit

---
+++ prReviewBot/src/main/java/com/proj/prreviewbot/PrReviewBotApplication.java
@@ -0,0 +1,13 @@
+package com.proj.prreviewbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrReviewBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrReviewBotApplication.class, args);
+    }
+
+}
---
+++ prReviewBot/src/test/java/com/proj/prreviewbot/PrReviewBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.prreviewbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrReviewBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
